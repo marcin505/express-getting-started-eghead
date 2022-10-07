055613dcf8524a93e77bc95d21f095d99bac726d
Marcin Galeziowski <marcin.galeziowski@gmail.com>
2022-10-07 15:33:08 +0200

    define dynamic routes through Express

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 var express = require('express');
+const res = require('express/lib/response');
 var app = express();
+var fs = require('fs');
+var _ = require('lodash');
+var users = [];
 
-app.get('/', function(req, res) {
-    res.send('Hello World')
+fs.readFile('users.json', {encoding: 'utf8'}, (err, data) => {
+  if (err) throw err
+
+  JSON.parse(data).forEach((user) => {
+    user.name.full = _.startCase(user.name.first + ' ' + user.name.last)
+    users.push(user)
+  })
+  
 });
 
-app.get('/yo', function(req, res) {
-    res.send('YO');
-});
+app.get('/', (req, res) => {
+  const buffer =  users.reduce((acc, cur) => acc+'<a href="/' + cur.username + '">' + cur.name.full + '</a><br>', '')
+  res.send(buffer)
+})
 
-var server = app.listen(3000, function() {
-    console.log(`Server at http://localhost:${server.address().port}`);
+app.get(/big.*/, (req, res, next) =>{
+  console.log('BIG USER ACCESS');
+  next();
+})
+app.get(/.*dog.*/, (req, res, next) =>{
+  console.log('DOGD GO WOOF');
+  next();
+})
+
+
+app.get('/:username', (req, res) => {
+  var username = req.params.username;
+  res.send(username);
+})
+
+var server = app.listen(3000, () => {
+  console.log(`Server at http://localhost:${server.address().port}`);
 });
 
