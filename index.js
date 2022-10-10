c8530fc5cfd5505c1e38a9b066f82273b7f0b3a2
Marcin Galeziowski <marcin.galeziowski@gmail.com>
2022-10-10 14:35:59 +0200

    HTTP Verbs

--- index.js
+++ index.js
@@ -1,42 +1,83 @@
-var express = require('express');
-const res = require('express/lib/response');
-var app = express();
-var fs = require('fs');
-var _ = require('lodash');
-var engine = require('consolidate');
+var express = require('express')
+var app = express()
 
+var fs = require('fs')
+var path = require('path')
+var _ = require('lodash')
+var engines = require('consolidate')
 
-var users = [];
+var bodyParser = require('body-parser')
 
-fs.readFile('users.json', {encoding: 'utf8'}, (err, data) => {
-  if (err) throw err
+function getUserFilePath (username) {
+  return path.join(__dirname, 'users', username) + '.json'
+}
 
-  JSON.parse(data).forEach((user) => {
-    user.name.full = _.startCase(user.name.first + ' ' + user.name.last)
-    users.push(user)
+function getUser (username) {
+  var user = JSON.parse(fs.readFileSync(getUserFilePath(username), {encoding: 'utf8'}))
+  user.name.full = _.startCase(user.name.first + ' ' + user.name.last)
+  _.keys(user.location).forEach(function (key) {
+    user.location[key] = _.startCase(user.location[key])
   })
-  
-});
+  return user
+}
 
-app.engine('hbs', engine.handlebars);
+function saveUser (username, data) {
+  var fp = getUserFilePath(username)
+  fs.unlinkSync(fp) // delete the file
+  fs.writeFileSync(fp, JSON.stringify(data, null, 2), {encoding: 'utf8'})
+}
 
-app.set('views', './views');
-app.set('view engine', 'hbs');
+app.engine('hbs', engines.handlebars)
 
-app.use('/profilepics', express.static('images'));
+app.set('views', './views')
+app.set('view engine', 'hbs')
 
-app.get('/', (req, res) => {
-  // res.send(buffer)
-  res.render('index', {users })
+app.use('/profilepics', express.static('images'))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/favicon.ico', function (req, res) {
+  res.end()
 })
 
-app.get('/:username', (req, res) => {
-  const username = req.params.username;
-  res.render('user', {username });
-  // res.send(username);
+app.get('/', function (req, res) {
+  var users = []
+  fs.readdir('users', function (err, files) {
+    files.forEach(function (file) {
+      fs.readFile(path.join(__dirname, 'users', file), {encoding: 'utf8'}, function (err, data) {
+        var user = JSON.parse(data)
+        user.name.full = _.startCase(user.name.first + ' ' + user.name.last)
+        users.push(user)
+        if (users.length === files.length) res.render('index', {users: users})
+      })
+    })
+  })
 })
 
-var server = app.listen(3000, () => {
-  console.log(`Server at http://localhost:${server.address().port}`);
-});
+app.get('/:username', function (req, res) {
+  var username = req.params.username
+  var user = getUser(username)
+  res.render('user', {
+    user: user,
+    address: user.location
+  })
+})
 
+app.put('/:username', function (req, res) {
+  var username = req.params.username
+  var user = getUser(username)
+  user.location = req.body;
+  console.log(user);
+  saveUser(username, user)
+  res.sendStatus(200);
+  res.end()
+})
+
+app.delete('/:username', function (req, res) {
+  var fp = geUtserFilePath(req.params.username)
+  fs.unlinkSync(fp) // delete the file
+  res.sendStatus(200)
+})
+
+var server = app.listen(3000, function () {
+  console.log('Server running at http://localhost:' + server.address().port)
+})
