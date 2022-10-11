d1f7ab95bb7597e7ee467b4930d8735bc78c2150
Marcin Galeziowski <marcin.galeziowski@gmail.com>
2022-10-11 09:16:03 +0200

    07 - Organize Code by Subpath in Express

--- index.js
+++ index.js
@@ -1,31 +1,13 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const app = express()
 
-var fs = require('fs')
-var path = require('path')
-var _ = require('lodash')
-var engines = require('consolidate')
+const fs = require('fs')
+const path = require('path')
+const _ = require('lodash')
+const engines = require('consolidate')
+const helpers = require('./helpers')
 
-var bodyParser = require('body-parser')
-
-function getUserFilePath (username) {
-  return path.join(__dirname, 'users', username) + '.json'
-}
-
-function getUser (username) {
-  var user = JSON.parse(fs.readFileSync(getUserFilePath(username), {encoding: 'utf8'}))
-  user.name.full = _.startCase(user.name.first + ' ' + user.name.last)
-  _.keys(user.location).forEach(function (key) {
-    user.location[key] = _.startCase(user.location[key])
-  })
-  return user
-}
-
-function saveUser (username, data) {
-  var fp = getUserFilePath(username)
-  fs.unlinkSync(fp) // delete the file
-  fs.writeFileSync(fp, JSON.stringify(data, null, 2), {encoding: 'utf8'})
-}
+const bodyParser = require('body-parser')
 
 app.engine('hbs', engines.handlebars)
 
@@ -35,21 +17,16 @@ app.set('view engine', 'hbs')
 app.use('/profilepics', express.static('images'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.all('/:username', function(req, res, next) {
-  console.log(req.method, 'for', req.params.username);
-  next();
-});
-
 app.get('/favicon.ico', function (req, res) {
   res.end()
 })
 
 app.get('/', function (req, res) {
-  var users = []
+  const users = []
   fs.readdir('users', function (err, files) {
     files.forEach(function (file) {
       fs.readFile(path.join(__dirname, 'users', file), {encoding: 'utf8'}, function (err, data) {
-        var user = JSON.parse(data)
+        const user = JSON.parse(data)
         user.name.full = _.startCase(user.name.first + ' ' + user.name.last)
         users.push(user)
         if (users.length === files.length) res.render('index', {users: users})
@@ -59,7 +36,7 @@ app.get('/', function (req, res) {
 })
 
 function verifyUser (req, res, next) {
-  var fp = getUserFilePath(req.params.username)
+  const fp = getUserFilePath(req.params.username)
   fs.exists(fp, function (yes) {
     if (yes) {
       next()
@@ -70,44 +47,23 @@ function verifyUser (req, res, next) {
   }
 
 app.get('*.json', function (req, res) {
-  res.download(`./users/${req.path}`, 'funny name.exe')
+  res.download(`./users/${req.path}`, 'funny-name.exe')
 });
 
-app.get('/:username',verifyUser, function (req, res) {
-  var username = req.params.username
-  var user = getUser(username)
-  res.render('user', {
-    user: user,
-    address: user.location
-  })
-});
 
 app.get('/error/:username', function (req, res) {
   res.status(404).send(`No user name ${req.params.username} found`);
 });
 
 app.get(`/data/:username`, function (req, res) {
-  var username = req.params.username;
-  var user = getUser(username);
+  const username = req.params.username;
+  const user = helpers.getUser(username);
   res.json(user);
 })
 
-app.put('/:username', function (req, res) {
-  var username = req.params.username
-  var user = getUser(username)
-  user.location = req.body;
-  // console.log(user);
-  saveUser(username, user)
-  res.sendStatus(200);
-  res.end()
-})
+const userRouter = require('./username')
+app.use('/:username', userRouter)
 
-app.delete('/:username', function (req, res) {
-  var fp = geUtserFilePath(req.params.username)
-  fs.unlinkSync(fp) // delete the file
-  res.sendStatus(200)
-})
-
-var server = app.listen(3000, function () {
+const server = app.listen(3000, function () {
   console.log('Server running at http://localhost:' + server.address().port)
 })
---
+++ username.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const helpers = require('./helpers')
+const fs = require('fs')
+
+const router = express.Router({
+  mergeParams: true
+})
+
+router.all('/', function (req, res, next) {
+  console.log(req.method, 'for', req.params.username)
+  next()
+})
+
+router.get('/', helpers.verifyUser, function (req, res) {
+  const username = req.params.username
+  const user = helpers.getUser(username)
+  res.render('user', {
+    user: user,
+    address: user.location
+  })
+})
+ 
+router.get('/edit', function (req, res) {
+  res.send('You want to edit ' + req.params.username + '???')
+})
+
+router.put('/', function (req, res) {
+  const username = req.params.username
+  const user = helpers.getUser(username)
+  user.location = req.body
+  helpers.saveUser(username, user)
+  res.end()
+})
+
+router.delete('/', function (req, res) {
+  const fp = helpers.getUserFilePath(req.params.username)
+  fs.unlinkSync(fp) // delete the file
+  res.sendStatus(200)
+})
+
+module.exports = router
